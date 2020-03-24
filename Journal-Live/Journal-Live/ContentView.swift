e49da4785fc62c332c1399d7997b16ef2dc8097e
喧静阁 <lijiran312@sina.com>
2020-03-24 15:54:25 +0800

    初始化

---
+++ Journal-Live/Journal-Live/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Journal-Live
+//
+//  Created by Jiran Li on 2020/3/24.
+//  Copyright © 2020 Jiran Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
